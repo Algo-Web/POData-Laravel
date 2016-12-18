e4c94ee62f7a994ed1df5e5edf554dd707a88805
Alex Goodwin <cyberia.resurrection@gmail.com>
2016-12-18 20:30:01 +1000

    Add metadata controller to test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,8 @@ use Mockery;
 use PHPUnit_Framework_TestCase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use POData\Providers\Metadata\SimpleMetadataProvider;
+use AlgoWeb\PODataLaravel\Controllers\MetadataControllerContainer;
+
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\vfsStreamWrapper;
 use org\bovigo\vfs\vfsStreamDirectory;
@@ -41,7 +43,7 @@ class TestCase extends BaseTestCase
         $app = new \AlgoWeb\PODataLaravel\Models\TestApplication($fileSys);
         $app['env'] = 'testing';
         $app->instance('config', $confRepo);
-        $app->config->set('app.providers', []);
+        $app->config->set('app.providers', [\AlgoWeb\PODataLaravel\Providers\MetadataControllerProvider::class]);
         $app->instance('cache.store', $cacheRepo);
         $app->instance('db', $database);
         $app->instance('log', $log);
@@ -70,6 +72,10 @@ class TestCase extends BaseTestCase
                 return new SimpleMetadataProvider('Data', 'Data');
             }
         );
+
+        $app->singleton('metadataControllers', function ($app) {
+            return new MetadataControllerContainer();
+        });
 
         return $app;
     }
