da0c25f603ac10226b2ac71935c78f443d76e69f
Alex Goodwin <cyberia.resurrection@gmail.com>
2017-06-09 22:23:09 +1000

    Expand AuthInterface to handle relations as well as models

--- src/Auth/NullAuthProvider.php
+++ src/Auth/NullAuthProvider.php
@@ -5,10 +5,19 @@ namespace AlgoWeb\PODataLaravel\Auth;
 use AlgoWeb\PODataLaravel\Interfaces\AuthInterface;
 use Illuminate\Database\Eloquent\Model;
 use AlgoWeb\PODataLaravel\Enums\ActionVerb;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class NullAuthProvider implements AuthInterface
 {
-    public function canAuth(ActionVerb $verb, $modelname, Model $model = null)
+    /**
+     * Is the requester permitted to perform the requested action on the model class (and instance, if supplied)?
+     *
+     * @param ActionVerb $verb
+     * @param $modelname  Model class to access
+     * @param Model|Relation|null $model  Specific model or relation to access
+     * @return bool
+     */
+    public function canAuth(ActionVerb $verb, $modelname, $model = null)
     {
         return true;
     }
