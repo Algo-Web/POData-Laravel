207c1a51506ae1ec153d1cdc9cc362fd419d7382
c-harris <c.harris@communicationinnovations.com.au>
2017-09-24 20:45:31 +1000

    Apply fixes from StyleCI (#39)

--- src/Providers/MetadataProvider.php
+++ src/Providers/MetadataProvider.php
@@ -293,7 +293,7 @@ class MetadataProvider extends MetadataBaseProvider
     /**
      * Bootstrap the application services.  Post-boot.
      *
-     * @param  mixed $reset
+     * @param mixed $reset
      *
      * @return void
      */
