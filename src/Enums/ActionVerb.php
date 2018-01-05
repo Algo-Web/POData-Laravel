42681cedc584e02dae8fde81b71d7e7001390519
Alex Goodwin <cyberia.resurrection@gmail.com>
2018-01-05 19:50:44 +1000

    Add static-method annotations to ActionVerb enum

--- src/Enums/ActionVerb.php
+++ src/Enums/ActionVerb.php
@@ -4,6 +4,12 @@ namespace AlgoWeb\PODataLaravel\Enums;
 
 use MyCLabs\Enum\Enum;
 
+/**
+ * @method static ActionVerb READ()
+ * @method static ActionVerb CREATE()
+ * @method static ActionVerb UPDATE()
+ * @method static ActionVerb DELETE()
+ */
 class ActionVerb extends Enum
 {
     const CREATE = 'create';
