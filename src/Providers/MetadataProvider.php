f6a3dae5602f73fdeaf1c40d61777ae9f285f765
Noel De Martin <noeldemartin@gmail.com>
2018-06-10 12:21:44 +0200

    Add laravel namespace helper

--- src/Providers/MetadataProvider.php
+++ src/Providers/MetadataProvider.php
@@ -311,7 +311,7 @@ class MetadataProvider extends MetadataBaseProvider
     {
         $classes = $this->getClassMap();
         $ends = [];
-        $startName = defined('PODATA_LARAVEL_APP_ROOT_NAMESPACE') ? PODATA_LARAVEL_APP_ROOT_NAMESPACE : 'App';
+        $startName = $this->app->getNamespace();
         foreach ($classes as $name) {
             if (\Illuminate\Support\Str::startsWith($name, $startName)) {
                 if (in_array('AlgoWeb\\PODataLaravel\\Models\\MetadataTrait', class_uses($name)) &&
