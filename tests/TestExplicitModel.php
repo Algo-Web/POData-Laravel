2c8796d5571c8f532700c8c1c1aa3d0dc5bbe87c
Noel De Martin <noeldemartin@gmail.com>
2018-06-10 13:47:37 +0200

    Add model for explicit metadata definition tests

---
+++ tests/TestExplicitModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AlgoWeb\PODataLaravel\Models;
+
+use AlgoWeb\PODataLaravel\Models\MetadataTrait;
+use Illuminate\Database\Eloquent\Model as Model;
+use AlgoWeb\PODataLaravel\Models\ObjectMap\Entities\EntityFieldPrimitiveType;
+
+class TestExplicitModel extends Model
+{
+    use MetadataTrait;
+
+    protected $odata = [
+        'id' => [
+            'type' => EntityFieldPrimitiveType::INTEGER,
+            'nullable' => false,
+            'fillable' => false,
+            'default' => null,
+        ],
+        'name' => [
+            'type' => EntityFieldPrimitiveType::STRING,
+            'nullable' => false,
+            'fillable' => true,
+            'default' => null,
+        ],
+    ];
+}
