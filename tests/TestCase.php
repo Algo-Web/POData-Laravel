6afb47764b8974a4af2768b2dacaef2f2ead3c60
Alex Goodwin <cyberia.resurrection@gmail.com>
2018-07-16 02:00:50 +1000

    Bolt supplied providers into test suite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,27 @@
 
 namespace AlgoWeb\PODataLaravel\Models;
 
+use AlgoWeb\PODataLaravel\Kernels\ConsoleKernel;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-    //
+    protected function getPackageProviders($app)
+    {
+        return [\AlgoWeb\PODataLaravel\Providers\MetadataProvider::class,
+            \AlgoWeb\PODataLaravel\Providers\MetadataRouteProvider::class,
+            \AlgoWeb\PODataLaravel\Providers\QueryProvider::class,
+            \AlgoWeb\PODataLaravel\Providers\MetadataControllerProvider::class];
+    }
+
+    /**
+     * Resolve application Console Kernel implementation.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function resolveApplicationConsoleKernel($app)
+    {
+        $app->singleton('Illuminate\Contracts\Console\Kernel', ConsoleKernel::class);
+    }
 }
