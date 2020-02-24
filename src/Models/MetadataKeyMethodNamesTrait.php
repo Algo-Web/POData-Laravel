719865561e1f0824153bd3295da1e817bff1345f
c-harris <c.harris@communicationinnovations.com.au>
2020-02-25 01:58:24 +1000

    changed to seperate methods for readability

--- src/Models/MetadataKeyMethodNamesTrait.php
+++ src/Models/MetadataKeyMethodNamesTrait.php
@@ -20,42 +20,46 @@ trait MetadataKeyMethodNamesTrait
 {
     protected function polyglotFkKey(Relation $rel)
     {
-        return $this->polyglotKey($rel, true);
-    }
-
-    protected function polyglotRkKey(Relation $rel)
-    {
-        return $this->polyglotKey($rel, false);
+        switch (true) {
+            case $rel instanceof BelongsTo:
+                $key =  $rel->{$this->checkMethodNameList($rel, ['getForeignKeyName', 'getForeignKey'])}();
+                break;
+            case $rel instanceof BelongsToMany:
+                $key = $rel->getForeignPivotKeyName();
+                break;
+            case $rel instanceof HasOneOrMany:
+                $key = $rel->getForeignKeyName();
+                break;
+            case $rel instanceof HasManyThrough:
+                $key =  $rel->getQualifiedFarKeyName();
+                break;
+            default:
+                $msg = sprintf('Unknown Relationship Type %s', get_class($rel));
+                throw new InvalidOperationException($msg);
+        }
+        $segments = explode('.', $key);
+        return end($segments);
     }
 
     /**
-     * @param $rel
-     * @param $fk
+     * @param Relation $rel
      * @return mixed
      * @throws InvalidOperationException
      */
-    protected function polyglotKey($rel, $fk)
+    protected function polyglotRkKey(Relation $rel)
     {
         switch (true) {
             case $rel instanceof BelongsTo:
-                $key = $fk ?
-                    $rel->{$this->checkMethodNameList($rel, ['getForeignKeyName', 'getForeignKey'])}() :
-                    $rel->{$this->checkMethodNameList($rel, ['getOwnerKey', 'getOwnerKeyName'])}();
+                $key = $rel->{$this->checkMethodNameList($rel, ['getOwnerKey', 'getOwnerKeyName'])}();
                 break;
             case $rel instanceof BelongsToMany:
-                $key = $fk ?
-                    $rel->getForeignPivotKeyName() :
-                    $rel->getRelatedPivotKeyName();
+                $key = $rel->getRelatedPivotKeyName();
                 break;
             case $rel instanceof HasOneOrMany:
-                $key = $fk ?
-                    $rel->getForeignKeyName() :
-                    $rel->{$this->checkMethodNameList($rel, ['getLocalKeyName', 'getQualifiedParentKeyName'])}();
+                $key = $rel->{$this->checkMethodNameList($rel, ['getLocalKeyName', 'getQualifiedParentKeyName'])}();
                 break;
             case $rel instanceof HasManyThrough:
-                $key = $fk ?
-                    $rel->getQualifiedFarKeyName() :
-                    $rel->getQualifiedParentKeyName();
+                $key = $rel->getQualifiedParentKeyName();
                 break;
             default:
                 $msg = sprintf('Unknown Relationship Type %s', get_class($rel));
@@ -83,10 +87,10 @@ trait MetadataKeyMethodNamesTrait
      */
     protected function getModelClassMethods(Model $model)
     {
+        // TODO: Handle case when Mock::class not present
         return array_diff(
             get_class_methods($model),
             get_class_methods(\Illuminate\Database\Eloquent\Model::class),
-            //TODO: sandi what will happen if Mock is not installed (I.e. Production?)
             get_class_methods(Mock::class),
             get_class_methods(MetadataTrait::class)
         );
