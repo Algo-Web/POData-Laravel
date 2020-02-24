56f8ee430aeaa1a55c905c9e898076683ee42467
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-02-24 16:35:12 +1000

    Fill most mutation holes in metadata controller provider

---
+++ tests/Orchestra/Controllers/OrchestraNonTraitController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 24/02/20
+ * Time: 4:00 PM
+ */
+
+namespace AlgoWeb\PODataLaravel\Orchestra\Tests\Controllers;
+
+use AlgoWeb\PODataLaravel\Controllers\Controller;
+
+class OrchestraNonTraitController extends Controller
+{
+}
---
+++ tests/Orchestra/Providers/DummyMetadataControllerProvider.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 24/02/20
+ * Time: 3:52 PM
+ */
+
+namespace AlgoWeb\PODataLaravel\Orchestra\Tests\Providers;
+
+use AlgoWeb\PODataLaravel\Providers\MetadataControllerProvider;
+
+class DummyMetadataControllerProvider extends MetadataControllerProvider
+{
+    /**
+     * @param  array      $classes
+     * @throws \Exception
+     * @return array
+     */
+    public function getCandidateControllers(array $classes)
+    {
+        return parent::getCandidateControllers($classes);
+    }
+}
