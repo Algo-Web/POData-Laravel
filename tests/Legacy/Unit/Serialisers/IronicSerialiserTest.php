894cd9103cec4c5a58174ec2974c88aabd02740a
c-harris <c.harris@communicationinnovations.com.au>
2020-02-26 00:28:31 +1000

    Fix error related to coupling on the extraction with the seralisation

--- tests/Legacy/Unit/Serialisers/IronicSerialiserTest.php
+++ tests/Legacy/Unit/Serialisers/IronicSerialiserTest.php
@@ -750,17 +750,17 @@ class IronicSerialiserTest extends SerialiserTestBase
         $ironic->setPropertyExpansion('oneSource', false);
         $ironic->setPropertyExpansion('manyTarget', false);
         $ironic->setPropertyExpansion('oneTarget', false);
-
         $ironicResult = $ironic->writeTopLevelElements($collection);
+
         $this->assertEquals(3, count($ironicResult->entries));
-        $this->assertNull($ironicResult->entries[0]->links[0]->expandedResult);
-        $this->assertNull($ironicResult->entries[1]->links[0]->expandedResult);
-        $this->assertNull($ironicResult->entries[2]->links[0]->expandedResult);
-        $this->assertTrue($ironicResult->entries[0]->links[1]->expandedResult instanceof ODataFeed);
-        $this->assertTrue($ironicResult->entries[0]->links[1]->expandedResult instanceof ODataFeed);
-        $this->assertEquals(1, count($ironicResult->entries[0]->links[1]->expandedResult->entries));
-        $this->assertTrue($ironicResult->entries[2]->links[1]->expandedResult instanceof ODataFeed);
-        $this->assertEquals(1, count($ironicResult->entries[2]->links[1]->expandedResult->entries));
+        $this->assertNull($ironicResult->entries[0]->links[1]->expandedResult);
+        $this->assertNull($ironicResult->entries[1]->links[1]->expandedResult);
+        $this->assertNull($ironicResult->entries[2]->links[1]->expandedResult);
+        $this->assertTrue($ironicResult->entries[0]->links[0]->expandedResult instanceof ODataFeed);
+        $this->assertTrue($ironicResult->entries[0]->links[0]->expandedResult instanceof ODataFeed);
+        $this->assertEquals(1, count($ironicResult->entries[0]->links[0]->expandedResult->entries));
+        $this->assertTrue($ironicResult->entries[2]->links[0]->expandedResult instanceof ODataFeed);
+        $this->assertEquals(1, count($ironicResult->entries[2]->links[0]->expandedResult->entries));
     }
 
     public function testGetConcreteTypeFromAbstractTypeWhereAbstractHasNoDerivedTypes()
