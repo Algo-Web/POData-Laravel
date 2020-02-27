957b8062310618803b8405659cdd4da5dea62b0d
c-harris <c.harris@communicationinnovations.com.au>
2020-02-27 18:21:03 +1000

    style cleanup

--- tests/Northwind/Models/Tag.php
+++ tests/Northwind/Models/Tag.php
@@ -4,11 +4,12 @@
 namespace Tests\Northwind\AlgoWeb\PODataLaravel\Models;
 
 
+use AlgoWeb\PODataLaravel\Models\MetadataTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    use \AlgoWeb\PODataLaravel\Models\MetadataTrait;
+    use MetadataTrait;
     protected $table = 'tags';
 
     public $timestamps = false;
@@ -20,12 +21,11 @@ class Tag extends Model
 
     public function taggedEmployees()
     {
-        return $this->morphedByMany(Employee::class,'taggable', 'taggable_pivot');
+        return $this->morphedByMany(Employee::class, 'taggable', 'taggable_pivot');
     }
 
     public function taggedCustomer()
     {
-        return $this->morphedByMany(Customer::class,'taggable', 'taggable_pivot');
+        return $this->morphedByMany(Customer::class, 'taggable', 'taggable_pivot');
     }
-
 }
