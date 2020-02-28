63a38934f120af3ca32e7e50533f0fa75ab3bbac
c-harris <c.harris@communicationinnovations.com.au>
2020-02-29 01:33:59 +1000

    Improve marshaling to monomorphic

--- src/Models/ObjectMap/Entities/Associations/AssociationFactory.php
+++ src/Models/ObjectMap/Entities/Associations/AssociationFactory.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Str;
 
 abstract class AssociationFactory
 {
+    public static $marshalPolymorphics = true;
+
     /**
      * @param AssociationStubBase $stubOne
      * @param AssociationStubBase $stubTwo
@@ -35,28 +37,39 @@ abstract class AssociationFactory
         $first = $oneFirst === $twoFirst ? -1 === $stubOne->compare($stubTwo) : $oneFirst;
 
         $association = new AssociationMonomorphic();
-        if ($stubOne->getTargType() == null) {
-            $oldName = $stubOne->getRelationName();
-            //$stubOne->addAssociation($association);
-            $stubOne = clone $stubOne;
-            $relPolyTypeName = substr($stubTwo->getBaseType(), strrpos($stubTwo->getBaseType(), '\\')+1);
-            $relPolyTypeName = Str::plural($relPolyTypeName, 1);
-            $stubOne->setRelationName($stubOne->getRelationName() . '_' . $relPolyTypeName);
-            $stubOne->setTargType($stubTwo->getBaseType());
-            $stubOne->setForeignFieldName($stubTwo->getKeyFieldName());
-            $entity = $stubOne->getEntity();
-            $stubs = $entity->getStubs();
-            if (array_key_exists($oldName, $stubs)) {
-                //    unset($stubs[$oldName]);
-            }
-            $stubs[$stubOne->getRelationName()] = $stubOne;
-            $entity->setStubs($stubs);
+        if (null === $stubTwo->getTargType()) {
+            dd($stubTwo);
+        }
+        if ($stubOne->getTargType() == null && self::$marshalPolymorphics) {
+            $stubOne = self::marshalPolyToMono($stubOne, $stubTwo);
         }
         $input[intval(!$first)] = $stubOne;
         $input[intval($first)] = $stubTwo;
         $association->setFirst($input[0]);
         $association->setLast($input[1]);
         return $association;
+    }
+
+    private static function marshalPolyToMono(
+        AssociationStubBase $stub,
+        AssociationStubBase $stubTwo
+    ): AssociationStubBase {
+        $oldName = $stub->getRelationName();
+        //$stubOne->addAssociation($association);
+        $stubNew = clone $stub;
+        $relPolyTypeName = substr($stubTwo->getBaseType(), strrpos($stubTwo->getBaseType(), '\\')+1);
+        $relPolyTypeName = Str::plural($relPolyTypeName, 1);
+        $stubNew->setRelationName($stub->getRelationName() . '_' . $relPolyTypeName);
+        $stubNew->setTargType($stubTwo->getBaseType());
+        $stubNew->setForeignFieldName($stubTwo->getKeyFieldName());
+        $entity = $stub->getEntity();
+        $stubs = $entity->getStubs();
+        if (array_key_exists($oldName, $stubs)) {
+               //unset($stubs[$oldName]);
+        }
+        $stubs[$stubNew->getRelationName()] = $stubNew;
+        $entity->setStubs($stubs);
+        return $stubNew;
     }
 
     private static function checkAssociations(
