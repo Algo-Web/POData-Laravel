6bba5d54c6010b0c3749a080dbd47b8c35a2121b
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-03-05 11:42:20 +1000

    Pay attention to return type hint

--- src/Models/ObjectMap/Entities/EntityGubbins.php
+++ src/Models/ObjectMap/Entities/EntityGubbins.php
@@ -176,7 +176,7 @@ class EntityGubbins
     {
         if ($association instanceof AssociationMonomorphic) {
             $stub = $isFirst ? $association->getFirst() : $association->getLast();
-            if (null === $stub || (!in_array($stub, $this->stubs) && !($stub instanceof AssociationStubPolymorphic))) {
+            if ((!in_array($stub, $this->stubs) && !($stub instanceof AssociationStubPolymorphic))) {
                 throw new \InvalidArgumentException('Association cannot be connected to this entity');
             }
             $propertyName = $stub->getRelationName();
